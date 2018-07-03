4f615b54dc372ed42828242e6bcebab727a36cf5
Weichun <weichun@csie.tw>
2018-07-03 23:29:54 +0800

    change bullet draw position

--- src/js/user/Bullet.js
+++ src/js/user/Bullet.js
@@ -47,7 +47,7 @@ var user = (function (user) {
                 ctx.fillStyle = '#F0F';
             }
 
-            ctx.fillRect(this.position.x, this.position.y - BULLET_HEIGHT / 2, BULLET_WIDTH, BULLET_HEIGHT);
+            ctx.fillRect(this.position.x, this.position.y, BULLET_WIDTH, BULLET_HEIGHT);
 
             ctx.fillStyle = oldFillColor;
 
